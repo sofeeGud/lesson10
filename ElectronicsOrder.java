92c5a13d5fe0a8d1729c32bf25fa78e49e0e6ca3
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-07-08 21:15:49 +0300

    lesson10

--- ElectronicsOrder.java
+++ ElectronicsOrder.java
@@ -12,17 +12,16 @@ public class ElectronicsOrder extends Order {
 
     @Override
     void validateOrder() {
-
         if (getCustomerOwned() != null) {
             if (getShipFromCity() == "Киев" || getShipFromCity() == "Одесса" || getShipFromCity() == "Днепр" || getShipFromCity() == "Харьков") {
-                //if (getShipToCity() == "Киев" || getShipToCity() == "Одесса" || getShipToCity() == "Днепр" || getShipToCity() == "Харьков") {
+                if (getShipToCity() == "Киев" || getShipToCity() == "Одесса" || getShipToCity() == "Днепр" || getShipToCity() == "Харьков") {
                     if (getTotalPrice() <= 100) {
-                        //if (getCustomerOwned().getName() != null) {
+                        if (getCustomerOwned().getName() != null) {
                             if (getCustomerOwned().getGender() == "Женский") {
                                 setDateConfirmed(new Date());
                             }
-                        //}
-                   // }
+                        }
+                    }
                 }
 
             }
