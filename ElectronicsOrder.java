16909a74075a24d33e2ea2cc990b07607d9be5c5
Lenovo T540p <sonychkalapochka94@gmail.com>
2019-07-08 22:50:14 +0300

    lesson10

--- ElectronicsOrder.java
+++ ElectronicsOrder.java
@@ -64,15 +64,15 @@ public class ElectronicsOrder extends Order {
     @Override
     public void calculatePrice() {
         double sumShip = getBasePrice();
-        if (getShipToCity() != getShipFromCity()) {
+
             if (getShipToCity() == "Киев" || getShipToCity() == "Одесса") {
                 sumShip *= 1.10;
-            } else {
+            } else if (getShipToCity() != getShipFromCity()){
                 sumShip *= 1.15;
             }
-        }
 
-        if (sumShip > 1000) {
+
+        if (getBasePrice() > 1000) {
             sumShip *= 0.95;
         }
 
